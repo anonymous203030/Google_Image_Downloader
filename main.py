21ec70735dec446313d54e1ed7c35f82bcda5980
anonymous203030 <aram.simonyan.03@gmail.com>
2021-01-08 15:22:51 +0400

    Added Sqlite3 as Database which contains urls,for making program not to download same images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 import urllib.request
 import time
 import magic
@@ -9,7 +10,7 @@ class ImageDownloader:
     def __init__(self):
         pass
 
-    def GetUrls(self, keyword_to_search, limit, extensions=None):
+    def GetUrls(self, keyword_to_search, start, limit, extensions=None):
         if extensions == None:
             extensions = {'.jpg', '.png', '.ico', '.gif', '.jpeg'}
         links = []
@@ -31,7 +32,6 @@ class ImageDownloader:
             raw_html = self.DownloadPage(url)
             print('DOWNLOAD PAGE TIME:', time.time() - download_page_time)
             end_object = -1
-            google_image_seen = False
             j = 0
 
             while j < limit:
@@ -39,46 +39,50 @@ class ImageDownloader:
                 while True:
                     try:
 
-                        new_line = raw_html.find('"https://', end_object + 1)
-                        end_object = raw_html.find('"', new_line + 1)
-                        buffor = raw_html.find('\\', new_line + 1, end_object)
+                            new_line = raw_html.find('"https://', end_object + 1)
+                            end_object = raw_html.find('"', new_line + 1)
+                            buffor = raw_html.find('\\', new_line + 1, end_object)
 
-                        if buffor != -1:
-                            object_raw = (raw_html[new_line + 1:buffor])
-                        else:
-                            object_raw = (raw_html[new_line + 1:end_object])
+                            if buffor != -1:
+                                object_raw = (raw_html[new_line + 1:buffor])
+                            else:
+                                object_raw = (raw_html[new_line + 1:end_object])
 
-                        if any(extension in object_raw for extension in extensions):
+                            if any(extension in object_raw for extension in extensions):
+                                break
+                        except Exception as e:
                             break
-                    except Exception as e:
-                        break
-                print('RAW HTML FIND TIME', time.time() - raw_html_time)
-                photo_url_time = time.time()
-                path = word.replace(" ", "_")
-                print(path)
-                try:
-                    print(f'Photo {j} of {word} starting....')
-                    r = requests.get(object_raw, allow_redirects=True, timeout=0.3)
-                    if ('html' not in str(r.content)):
-                        mime = magic.Magic(mime=True)
-                        file_type = mime.from_buffer(r.content)
-                        file_extension = f'.{file_type.split("/")[1]}'
-                        if file_extension not in extensions:
-                            raise ValueError()
-                        if j == 0 or object_raw in spam:
-                            j += 1
-                            raise
-                        links.append(object_raw)
-                        print("PHOTO URL TIME", time.time() - photo_url_time)
-                        file_creating = time.time()
-                        dir_direction = str(word) + "_" + str(j) + file_extension
-                        with open(os.path.join(path, dir_direction), 'wb') as file:
-                            file.write(r.content)
-                            print(f'Photo {j} of {word} COMPLETED.GUT')
-                        print("FILE CREATING TIME", time.time() - file_creating)
-                except Exception as e:
-                    print("EXCEPTION", e)
-                    j -= 1
+                    all = self.DatabaseInside(object_raw)
+                    print(f'{len(all)} in Database.')
+                    if len(all) == 0:
+                        print('RAW HTML FIND TIME', time.time() - raw_html_time)
+                        photo_url_time = time.time()
+                        path = word.replace(" ", "_")
+                        print(path)
+                        try:
+                            print(f'Photo {j} of {word} starting....')
+                            r = requests.get(object_raw, allow_redirects=True, timeout=0.3)
+                            if ('html' not in str(r.content)):
+                                mime = magic.Magic(mime=True)
+                                file_type = mime.from_buffer(r.content)
+                                file_extension = f'.{file_type.split("/")[1]}'
+                                if file_extension not in extensions:
+                                    raise ValueError()
+                                if j == 0 or object_raw in spam:
+                                    j += 1
+                                    raise
+                                links.append(object_raw)
+                                print("PHOTO URL TIME", time.time() - photo_url_time)
+                                file_creating = time.time()
+                                dir_direction = str(word) + "_" + str(j) + file_extension
+                                with open(os.path.join(path, dir_direction), 'wb') as file:
+                                    file.write(r.content)
+                                    self.DatabaseConnect(object_raw, word)
+                                    print(f'Photo {j} of {word} COMPLETED.GUT')
+                                print("FILE CREATING TIME", time.time() - file_creating)
+                        except Exception as e:
+                            print("EXCEPTION", e)
+                            j -= 1
                 j += 1
         return links, keyword_to_search
 
@@ -95,6 +99,28 @@ class ImageDownloader:
         except Exception as e:
             print('ERRORROROROROORORRR', e)
             exit(0)
+
+    def DatabaseConnect(self, urls, keyword):
+        print('Including into Database')
+        time0 = time.time()
+        conn = sqlite3.connect('Images.db')
+
+        conn.execute("INSERT INTO ImageUrls (URL, NAME)"
+                     f"VALUES ('{urls}', '{keyword}')")
+        conn.commit()
+
+        conn.close()
+        return print('Ended Dbase including', time.time() - time0)
+
+    def DatabaseInside(self, url):
+        print('Executing From Database')
+        time0 = time.time()
+        conn = sqlite3.connect('Images.db')
+        all = conn.execute(f'SELECT URL FROM ImageUrls WHERE URL="{url}"')
+        all_urls = all.fetchall()
+        # conn.commit()
+        conn.close()
+        return all_urls
 
 
 if __name__ == '__main__':
